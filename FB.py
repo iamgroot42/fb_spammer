67f720937413da845fb059c8cb38c01426705284
Anshuman Suri <anshuman14021@iiitd.ac.in>
2015-07-31 19:29:45 +0530

    Update FB.py

--- FB.py
+++ FB.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 import time
 import facebook #sudo pip install facebook
@@ -27,16 +28,7 @@ graph = facebook.GraphAPI(token)
 go=str(raw_input("Enter event link : "))
 # go="https://www.facebook.com/events/1596690117274104/"
 print "Enter message :"
-temp=str(raw_input())
-mess=""
-i=0
-while temp!="potato":
-  if(i==1):
-    mess=mess+'\n'+temp
-  else:
-    mess=temp
-    i=1 
-  temp=str(raw_input())
+mess=str(raw_input())
 # mess="Hurry! Register today before midnight at esya.iiitd.edu.in/pcj"
 # profile = graph.get_object("IEEE.org")
 # print profile['name']
